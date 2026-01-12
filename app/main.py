a04c091d05def5b51991d970da0e135a30e03f3b
Ambra <trank13f@gmail.com>
2026-01-12 14:10:39 +0100

    file copy

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, src, dst = parts
+
+    if src == dst:
+        return
+
+    if not os.path.exists(src):
+        return
+
+    try:
+        with open(src, "r") as file_in, open(dst, "w") as file_out:
+            file_out.write(file_in.read())
+    except OSError:
+        return
