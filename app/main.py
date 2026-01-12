aa9f5962da9f128de12ae4c11c636865236c0be6
Ambra <trank13f@gmail.com>
2026-01-12 14:18:36 +0100

    .../.

--- app/main.py
+++ app/main.py
@@ -16,7 +16,8 @@ def copy_file(command: str) -> None:
         return
 
     try:
-        with open(source_file, "r") as file_in, open(destination_file, "w") as file_out:
+        with open(source_file, "r") as file_in,\
+                open(destination_file, "w") as file_out:
             file_out.write(file_in.read())
     except OSError:
         return
